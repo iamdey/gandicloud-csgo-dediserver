e4255c9dcf89e15a7d070c95e0390ba1a3082f44
David Epely <esion99@gmail.com>
2022-01-06 10:22:10 +0100

    add IP to .env file

--- bin/install-server.sh
+++ bin/install-server.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-IP=
+if [ -f .env ]; then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+if [ -z ${IP} ]; then
+  echo "  IP of the server not set. Please install it and fill it in .env"
+  exit 1
+fi
 
 scp host/* root@${IP}:/root/
 
