223ec351d58df36e6ed32b7731c28e327a82c111
David Epely <esion99@gmail.com>
2022-01-12 00:51:34 +0100

    [server-manager-api] implem host/state get & post

--- server-manager-api/src/index.js
+++ server-manager-api/src/index.js
@@ -1,12 +1,17 @@
 // @ts-check
 
+const util = require('util');
 const express = require('express');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const exec = util.promisify(require('child_process').exec);
+
 const app = express();
 const port = 3000;
 const TOKEN = 'foobar';
 
 app.use(helmet());
+app.use(bodyParser.json());
 
 function authorization(req, res, next) {
   const bearer = req.get('Bearer');
@@ -19,11 +24,46 @@ function authorization(req, res, next) {
 
 app.use(authorization);
 
-app.get('/host/state', (req, res) => {
-  res.send('return current state');
+async function getHostState() {
+  try {
+    await exec(`ping ${SERVER_IP} -c 1 -W 10`);
+
+    return 'alive';
+  } catch (err) {
+    return 'offline';
+  }
+}
+
+app.get('/host/state', async (req, res) => {
+  const state = await getHostState();
+  return res.json({ state });
 });
-app.post('/host/state', (req, res) => {
-  res.send('change state');
+
+app.post('/host/state', async (req, res) => {
+  const { action } = req.body;
+  if (!['boot', 'shutdown'].includes(action)) {
+    return res.status(400).json({ err: `Unknown action "${action}"` });
+  }
+
+  const state = await getHostState();
+  if (state === 'alive' && action === 'boot') {
+    return res.status(400).json({ err: 'Host already started' });
+  }
+  if (state === 'offline' && action === 'shutdown') {
+    return res.status(400).json({ err: 'Host already stoped' });
+  }
+
+  const mapOpenstackCmd = {
+    boot: 'start',
+    shutdown: 'stop',
+  };
+
+  try {
+    await exec(`openstack server ${mapOpenstackCmd[action]} ${HOSTNAME}`);
+  } catch (err) {
+    console.error(err.toString());
+    return res.status(500).send('Unable to change host state');
+  }
 });
 
 app.get('/', (req, res) => {
