0f1de3604f3451961cc72d9e6899ab169a29e1a4
David Epely <esion99@gmail.com>
2022-01-28 08:25:13 +0100

    reorganize config env file
    add openstack application creds creation

--- bin/read-config.sh
+++ bin/read-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -o allexport
-[[ -f config/.env.main ]] && source .env
+[[ -f config/main.env ]] && source main.env
 set +o allexport
 
 echo 'Please enter your GOStack Password: (same password as your gandi v5 account) '
